6b2f7864da8267fe5083a55060d3647dc9699a7a
Joel Lama <jlama1411@gmail.com>
2025-02-27 11:10:02 -0400

    Agregar la anotación @OA\Info

---
+++ backend/app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *     title="API Posts & Comments",
+ *     version="1.0.0",
+ *     description="Documentación de la API para gestión de posts y comentarios",
+ *     @OA\Contact(email="admin@example.com"),
+ *     @OA\License(name="MIT")
+ * )
+ * 
+ * @OA\Server(
+ *     url="http://localhost",
+ *     description="Servidor Local"
+ * )
+ */
+
+class SwaggerController extends Controller
+{
+    //
+}
